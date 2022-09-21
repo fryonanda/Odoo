eb74f4ee3f79e3496724c55002776aa7a85b7cff
Harfebi Fryonanda <114045608+fryonanda@users.noreply.github.com>
2022-09-21 11:14:10 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Academic Information Kelas MIB",
+    "version": "1.0.0", 
+    "depends": [
+        "base",
+    ],
+    "author": "Harfebi@pnp.ac.id", 
+    "category": "Education", 
+    'website': 'pnp.ac.id',
+    "description": """Latihan Addons Kelas MIB""",
+
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+
+
+  #  "data": [
+  #  "menu.xml",
+  #  ],
+}
