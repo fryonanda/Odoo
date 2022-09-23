c249ce0c4f845822b1fd47f70404a7ae144c914d
Harfebi Fryonanda <114045608+fryonanda@users.noreply.github.com>
2022-09-23 09:42:14 +0700

    Update course.py

--- Pertemuan 1/course.py
+++ Pertemuan 1/course.py
@@ -1,1 +1,14 @@
+from odoo import api, fields, models, _
 
+class Course(models.Model):
+    _name = 'academic.course'
+    _rec_name = 'name'
+
+    name = fields.Char("Name")
+    description = fields.Text(string="Description", required=False, )
+    responsible_id = fields.Many2one(comodel_name="res.users",
+                                     string="Responsible")
+    session_ids = fields.One2many(comodel_name="academic.session",
+                                  inverse_name="course_id",
+                                  string="Sessions", required=False,
+                                  ondelete="cascade")
