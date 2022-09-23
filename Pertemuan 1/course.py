fd3aa031b446493015ccc06eae691634df4f42e4
Harfebi Fryonanda <114045608+fryonanda@users.noreply.github.com>
2022-09-23 15:08:14 +0700

    Update course.py

--- Pertemuan 1/course.py
+++ Pertemuan 1/course.py
@@ -8,3 +8,8 @@ class Course(models.Model):
     description = fields.Text(string="Description", required=False, )
     responsible_id = fields.Many2one(comodel_name="res.users",
                                      string="Responsible")
+    
+    session_ids = fields.One2many(comodel_name="academic.session",
+                                  inverse_name="course_id",
+                                  string="Sessions", required=False,
+                                  ondelete="cascade")
